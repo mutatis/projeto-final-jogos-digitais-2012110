a98b7071b9c1a90fe8acdabb2221d7afb06a7ff9
rukiz917@gmail.com <rukiz917@gmail.com@d2ed95d1-07e2-9aaa-7ee2-0240a69621fd>
2013-05-24 00:28:13 +0000

    Fall 100% pronto. -Kaito

--- gameportal_v03.2/js/game2/game_2_scene_intro.js
+++ gameportal_v03.2/js/game2/game_2_scene_intro.js
@@ -5,7 +5,7 @@ function Game2SceneIntro()
 
 	//----------- intro musica----------
 	this.intro_musica = new Audio();
-	this.intro_musica.src = "sounds/game2/tela de inicar Cipher2.mp3";
+	this.intro_musica.src = "sounds/game2/menu_fall.mp3";
 	this.intro_musica.load();
 	this.intro_musica.loop = true;
 	this.intro_musica.volume = 1;	
