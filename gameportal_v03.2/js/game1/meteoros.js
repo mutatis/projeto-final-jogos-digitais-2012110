6cbe350ccffef405a490c8b54b1b0351aab7195a
jeffersonreiscel@gmail.com <jeffersonreiscel@gmail.com@d2ed95d1-07e2-9aaa-7ee2-0240a69621fd>
2013-05-25 17:31:48 +0000

    jogo 1 parte-2/?

--- gameportal_v03.2/js/game1/meteoros.js
+++ gameportal_v03.2/js/game1/meteoros.js
@@ -2,7 +2,6 @@ function Meteor (file, size_x, size_y, points)
 {
 	this.img = new Image();
 	this.img.src = file;
-	this.visible = true;
 	this.size_x = size_x;
 	this.size_y = size_y;
 	this.points = points;
@@ -17,54 +16,38 @@ function Meteor (file, size_x, size_y, points)
    		this.position_x += this.velocity_x;
    		
    		this.position_y += this.velocity_y;
-   		
-   		
+   		 		
    		// quando ultrapasar o limite da tela chama "function voltar"
 		if(this.position_y > SCREENHEIGHT)
-    	{
-    		this.voltar();	
-    	};
+			{
+				this.voltar();
+				game1.level1.points -= 1;
+			};
 	
-    	if(this.position_x > SCREENWIDTH - this.size_x )
-    	{
-    		this.voltar();
-    	}  
+    	if(this.position_x > SCREENWIDTH)
+			{
+				this.voltar();
+			};  
 		
-		if( this.position_x < 0 /*- this.size_x*/ )
-    	{
-    		this.voltar();
-    	} 
-
-
-		
+		if( this.position_x < 0 - this.size_x)
+			{
+				this.voltar();
+			};		
    };
-   
-   
-   
-   
+      
    this.draw=function()
-   {
-		if(this.visible)
+	{		
 		screen.drawImage(this.img, this.position_x, this.position_y, this.size_x, this.size_y);
-   };
+	};
    
    this.mouse_down=function(mouse)
-   {
-   	 if(Collide(mouse.x-10, mouse.y-12, 1, 1, this.position_x, this.position_y, this.size_x, this.size_y))
+	{
+		if(Collide(mouse.x-10, mouse.y-12, 1, 1, this.position_x, this.position_y, this.size_x, this.size_y))
 				{
-					if(this.visible)	
-					{	
-
-						this.visible = false;
-						this.voltar();
-
-					}
-				}
-		
-		
-		console.log("mouse X" + mouse.x + " mouse Y " + mouse.y );
-
-   };
+					this.voltar();					
+				};		
+			//console.log("mouse X" + mouse.x + " mouse Y " + mouse.y );
+	};
    
  
    this.mouse_up=function(mouse)
@@ -72,8 +55,7 @@ function Meteor (file, size_x, size_y, points)
    	
    };
    
-   
-   
+    
    this.mouse_move=function(mouse)
    {
    	
@@ -81,12 +63,11 @@ function Meteor (file, size_x, size_y, points)
    
 	
 	this.voltar=function()
-   {   	 
-		this.position_y = -this.size_y;
-		this.position_x = Math.floor((Math.random()*(SCREENWIDTH - this.size_x))); 
-		this.velocity_y = Math.floor((Math.random()*10)+1);
-		this.visible = true;
-    } 
+		{   	 
+			this.position_y = -this.size_y;
+			this.position_x = Math.floor((Math.random()*(SCREENWIDTH - this.size_x))); 
+			this.velocity_y = Math.floor((Math.random()*10)+1);
+		}; 
     
     
     function Collide(x1, y1, w1, h1, x2, y2, w2, h2)
