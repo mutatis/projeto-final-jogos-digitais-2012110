6c3691eb7a96539edef6059187d0e99b0d966692
gabriel.souba@gmail.com <gabriel.souba@gmail.com@d2ed95d1-07e2-9aaa-7ee2-0240a69621fd>
2013-05-27 22:24:12 +0000

    Pontuação negativa do Point n Click não passa de 0 agora LOL

--- Teste Crafty/src/game.js
+++ Teste Crafty/src/game.js
@@ -46,6 +46,59 @@ Game = {
     // Player character, placed at 5, 5 on our grid
     Crafty.e('PlayerCharacter').at(5, 5);
  
+
+    
     
   }
 }
+
+    // An "Actor" is an entity that is drawn in 2D on canvas
+//  via our logical coordinate grid
+Crafty.c('Actor', {
+  init: function() {
+    this.requires('2D, Canvas, Grid');
+  },
+});
+ 
+// A Tree is just an Actor with a certain color
+Crafty.c('Tree', {
+  init: function() {
+    this.requires('Actor, Color, Solid')
+      .color('rgb(20, 125, 40)');
+  },
+});
+ 
+// A Bush is just an Actor with a certain color
+Crafty.c('Bush', {
+  init: function() {
+    this.requires('Actor, Color, Solid')
+      .color('rgb(20, 185, 40)');
+  },
+});
+ 
+// This is the player-controlled character
+Crafty.c('PlayerCharacter', {
+  init: function() {
+    this.requires('Actor, Fourway, Color, Collision')
+      .fourway(4)
+      .color('rgb(20, 75, 40)')
+      .stopOnSolids();
+  },
+ 
+  // Registers a stop-movement function to be called when
+  //  this entity hits an entity with the "Solid" component
+  stopOnSolids: function() {
+    this.onHit('Solid', this.stopMovement);
+ 
+    return this;
+  },
+ 
+  // Stops the movement
+  stopMovement: function() {
+    this._speed = 0;
+    if (this._movement) {
+      this.x -= this._movement.x;
+      this.y -= this._movement.y;
+    }
+  }
+});
